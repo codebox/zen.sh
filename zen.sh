a55fe3c78c5137a40c6017a3a8d8d27dec548275
rob <rob@codebox.org.uk>
2020-05-19 11:49:40 +0100

    read session length from command line

--- zen.sh
+++ zen.sh
@@ -2,6 +2,8 @@
 
 INTERVAL_BELL_URL=https://codebox.net/assets/audio/bell1.mp3
 ENDING_BELL_URL=https://codebox.net/assets/audio/bell2.mp3
+MINUTE_MARKER=.
+INTERVAL_MARKER=+
 
 function play {
     URL_VAR=${1}_URL
@@ -13,9 +15,15 @@ function play {
     afplay $LOCAL_FILE &
 }
 
-SESSION_LENGTH_MINUTES=25
+DEFAULT_SESSION_LENGTH_MINUTES=60
+SESSION_LENGTH_MINUTES=${1:-$DEFAULT_SESSION_LENGTH_MINUTES}
 INTERVAL_LENGTH_MINUTES=10
 MINUTES=0
+
+if [[ ! $SESSION_LENGTH_MINUTES =~ ^[1-9][0-9]*$ ]]; then
+    echo "Usage: $0 [minutes]"
+    exit 1
+fi
 
 clear
 play INTERVAL_BELL
@@ -23,15 +31,15 @@ play INTERVAL_BELL
 while :; do
     sleep 60
     MINUTES=$((MINUTES + 1))
-    if [ $MINUTES -eq $SESSION_LENGTH_MINUTES ]; then
-        echo "+"
+    if [[ $MINUTES -eq $SESSION_LENGTH_MINUTES ]]; then
+        echo $INTERVAL_MARKER
         break
     fi
-    if [ $(( MINUTES % INTERVAL_LENGTH_MINUTES )) -eq 0 ]; then
-        echo -n "+"
+    if [[ $(( MINUTES % INTERVAL_LENGTH_MINUTES )) -eq 0 ]]; then
+        echo -n $INTERVAL_MARKER
         play INTERVAL_BELL
     else
-        echo -n "."
+        echo -n $MINUTE_MARKER
     fi
 done
 
